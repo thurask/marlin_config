29708ba35debce1c970139b0c668daadbf44b724
thurask <thuraski@hotmail.com>
2022-08-05 15:58:35 -0400

    Update Configuration.h

--- Configuration.h
+++ Configuration.h
@@ -1492,13 +1492,13 @@
 
 // The size of the printable area
 #define X_BED_SIZE 235
-#define Y_BED_SIZE 235
+#define Y_BED_SIZE 231  // the Y-axis tensioner cuts off some printable area
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE + 10
+#define X_MAX_POS X_BED_SIZE + 10  // extra room to maneuver
 #define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 250
 //#define I_MIN_POS 0
