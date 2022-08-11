feb24a78e2532f79a68f95041bcd5a2f5a1a0349
thurask <thuraski@hotmail.com>
2022-08-11 12:17:26 -0400

    Update Configuration.h

--- Configuration.h
+++ Configuration.h
@@ -1161,7 +1161,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 99.50 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 99.27168 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
